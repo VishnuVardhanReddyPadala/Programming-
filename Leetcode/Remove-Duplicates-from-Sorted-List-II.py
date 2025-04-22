24ecbb7667c3cbef7d3415f3cf56a1349c6b7c20
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-04-22 15:35:48 +0530

    [82] [Time Beats: 61.66%] [Memory Beats: 53.41%] - LeetPush

---
+++ Leetcode/Remove-Duplicates-from-Sorted-List-II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def deleteDuplicates(self, head):
+        dummy = ListNode(0, head)
+        prev = dummy
+        current = head
+
+        while current:
+            if current.next and current.val == current.next.val:
+                while current.next and current.val == current.next.val:
+                    current = current.next
+                prev.next = current.next
+            else:
+                prev = prev.next
+            current = current.next
+
+        return dummy.next
