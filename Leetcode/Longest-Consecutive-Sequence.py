d6bc798799c4fb206faaaac06a9929c13023152a
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-04-26 21:14:41 +0530

    [128] [Time Beats: 72.55%] [Memory Beats: 84.42%] - LeetPush

---
+++ Leetcode/Longest-Consecutive-Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        numSet = set(nums)
+        longest = 0
+        for num in numSet:
+            if num - 1 not in numSet:
+                currentNum = num
+                currentStreak = 1
+                while currentNum + 1 in numSet:
+                    currentNum += 1
+                    currentStreak += 1
+                longest = max(longest, currentStreak)
+        return longest
