cba02dca9a67a80d7cb2b8d8262b9693e3c938f3
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-04-26 21:23:47 +0530

    [134] [Time Beats: 32.00%] [Memory Beats: 65.20%] - LeetPush

---
+++ Leetcode/Gas-Station.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        total_gas, total_cost, current_gas, start_index = 0, 0, 0, 0
+        
+        for i in range(len(gas)):
+            total_gas += gas[i]
+            total_cost += cost[i]
+            current_gas += gas[i] - cost[i]
+            
+            if current_gas < 0:
+                start_index = i + 1
+                current_gas = 0
+        
+        return start_index if total_gas >= total_cost else -1
