7ee4ccc05ebc678a8c84429f1df1a7c8cd205e0d
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-05-01 08:26:37 +0530

    [146] [Time Beats: 74.08%] [Memory Beats: 6.99%] - LeetPush

---
+++ Leetcode/LRU-Cache.py
@@ -0,0 +1,56 @@
+class DLinkedNode:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = {}
+        self.capacity = capacity
+        self.head = DLinkedNode()
+        self.tail = DLinkedNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        node = self.tail.prev
+        self._remove_node(node)
+        return node
+
+    def get(self, key):
+        node = self.cache.get(key)
+        if not node:
+            return -1
+        self._move_to_head(node)
+        return node.value
+
+    def put(self, key, value):
+        node = self.cache.get(key)
+        if node:
+            node.value = value
+            self._move_to_head(node)
+        else:
+            new_node = DLinkedNode(key, value)
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            if len(self.cache) > self.capacity:
+                tail = self._pop_tail()
+                del self.cache[tail.key]
