b0cd721ec6749a7a70d7fee5864bf0a891246b45
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-05-01 08:27:10 +0530

    [147] [Time Beats: 36.08%] [Memory Beats: 21.39%] - LeetPush

---
+++ Leetcode/Insertion-Sort-List.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def insertionSortList(self, head):
+        dummy = ListNode(0)
+        curr = head
+        while curr:
+            prev = dummy
+            while prev.next and prev.next.val < curr.val:
+                prev = prev.next
+            next_temp = curr.next
+            curr.next = prev.next
+            prev.next = curr
+            curr = next_temp
+        return dummy.next
