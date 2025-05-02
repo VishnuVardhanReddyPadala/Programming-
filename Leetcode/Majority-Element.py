662b6ddc7d411909726e765141fda4a25789af27
Vishnu Vardhan Reddy <vishnuvardhan6842@gmail.com>
2025-05-02 22:19:19 +0530

    [169] [Time Beats: 54.95%] [Memory Beats: 31.36%] - LeetPush

---
+++ Leetcode/Majority-Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums):
+        candidate, count = None, 0
+        for num in nums:
+            if count == 0:
+                candidate, count = num, 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
